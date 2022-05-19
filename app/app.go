6561d7b37ac11a28f95ab8f0f27dc499d5e46225
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-05-18 22:23:35 -0300

    fix: assignment

--- app/app.go
+++ app/app.go
@@ -49,7 +49,7 @@ func Start() {
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}/account", ah.NewAccount).Methods(http.MethodPost)
-	router.HandleFunc("/customers/{customer_id:[0-9]+}/account/{customer_id:[0-9]+}", ah.MakeTransaction).
+	router.HandleFunc("/customers/{customer_id:[0-9]+}/account/{account_id:[0-9]+}", ah.MakeTransaction).
 		Methods(http.MethodPost)
 
 	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
